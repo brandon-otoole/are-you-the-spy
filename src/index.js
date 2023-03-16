dd4ca0d4d3c88f8132a307842241565ae0c06225
Brandon O'Toole <otoolb@gmail.com>
2023-03-16 13:00:52 -0700

    Update display, remove react image

--- src/index.js
+++ src/index.js
@@ -85,12 +85,10 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-/* */
   <React.StrictMode>
    <Provider store = {store}>
     <RouterProvider router={router}/>
    </Provider>
-    /* */
   </React.StrictMode>
 );
 
